b5f5c466a7c0714df5e865e2ccbb38a8d94f45ca
thanhtoan2000 <lethanhtoan7899992@gmail.com>
2021-11-18 17:51:28 +0700

    Chanle

--- source.cpp
+++ source.cpp
@@ -12,6 +12,13 @@ int tich(int a, int b);
 {
 return a*b;
 }
+void chanLe(int a){
+ if( a % 2 == 0)
+	count <<"so chan";
+else{
+	count<<"so le";
+}
+}
 int main()
 {
 	int a, b;
@@ -31,6 +38,9 @@ int main()
  	if(phepToan=="*")
         count<<"Tich :"<<tich(a,b)<<endl;
 
+	cout << "Nhap so nguyen a: ";
+	cin >> a;
+ 	chanLe(a);
 	system("pause");
 	return 0;
 }
